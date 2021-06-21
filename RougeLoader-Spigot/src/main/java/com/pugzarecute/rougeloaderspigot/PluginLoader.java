ca80312de6cc486c00b8d2eeffe359109bae3239
PutoPug <62641095+PutoPug@users.noreply.github.com>
2021-06-21 12:52:24 +0530

    Update copyright notice and clean imports

--- RougeLoader-Spigot/src/main/java/com/pugzarecute/rougeloaderspigot/PluginLoader.java
+++ RougeLoader-Spigot/src/main/java/com/pugzarecute/rougeloaderspigot/PluginLoader.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2021 PugzAreCute
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 package com.pugzarecute.rougeloaderspigot;
 
 import org.bukkit.Bukkit;
@@ -8,7 +26,6 @@ import org.bukkit.plugin.InvalidDescriptionException;
 import org.bukkit.plugin.InvalidPluginException;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 
 public class PluginLoader implements CommandExecutor {
     @Override
